247781531f70234e4a186f02d9d119f5342ab16f
NayF89 <46524603+NayF89@users.noreply.github.com>
2021-05-19 11:06:34 +0200

    Index show commit

--- fleetApp/src/main/java/com/springBoot/fleetApp/controller/ApplicationController.java
+++ fleetApp/src/main/java/com/springBoot/fleetApp/controller/ApplicationController.java
@@ -1,4 +1,4 @@
-package com.springBoot.fleetApp;
+package com.springBoot.fleetApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
